8d8263e3913a589bb3de4fd7be392bb541b06a48
Rob Jonson <rob@hobbyistsoftware.com>
2020-07-14 10:35:45 +0100

    add webP comment

--- Sources/HSHelpers/Types/Filetypes.swift
+++ Sources/HSHelpers/Types/Filetypes.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public struct Filetypes {
     
+    //Doesn't include webP because Apple can't handle it
     public static let photo =  ["png", "jpg", "jpeg", "tiff", "bmp", "gif"]
     public static let raw = ["3fr","ari","arw","bay","crw","cr2","cr3","cap",
                       "dcs","dcr","dng","drf","eip","erf","fff","gpr","iiq",
