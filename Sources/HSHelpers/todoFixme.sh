0c8a8b28e654e309d01b7eb8f3c996313e5eb4b4
Rob Jonson <rob@hobbyistsoftware.com>
2020-07-14 10:34:48 +0100

    add instructions for TodoFixme

--- Sources/HSHelpers/todoFixme.sh
+++ Sources/HSHelpers/todoFixme.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Run with the following in a build script phase
+# time ${BUILD_DIR%%Build/Products}SourcePackages/checkouts/HSHelpers/Sources/HSHelpers/todoFixme.sh
+
 KEYWORDS="TODO|FIXME|WARNING|\?\?\?:|\!\!\!:"
 find "${SRCROOT}" -path "${SRCROOT}/Pods" -prune -o \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/ warning: \$1/"
