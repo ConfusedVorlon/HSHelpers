de6c65f303279e602ebb34569bfcb22c7c4fe8e2
Rob Jonson <rob@hobbyistsoftware.com>
2020-07-29 21:21:20 +0100

    annoyingly - initialiser has to be manually declared

--- Sources/HSUIKit/UIViewController+Email.swift
+++ Sources/HSUIKit/UIViewController+Email.swift
@@ -13,6 +13,12 @@ import UIKit
 import MessageUI
 
 public struct EmailAttachment {
+    public init(data: Data, mimeType: String, fileName: String) {
+        self.data = data
+        self.mimeType = mimeType
+        self.fileName = fileName
+    }
+    
     public let data:Data
     public let mimeType:String
     public let fileName:String
