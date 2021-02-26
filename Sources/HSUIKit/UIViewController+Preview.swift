50663a60504f201df6238e36bd502360fc9061a4
Rob Jonson <rob@hobbyistsoftware.com>
2021-02-26 21:05:40 +0000

    add CanShowPreview

---
+++ Sources/HSUIKit/UIViewController+Preview.swift
@@ -0,0 +1,51 @@
+//
+//  UIDocumentInteractionController+HS.swift
+//  Rhythm101
+//
+//  Created by Rob Jonson on 13/12/2018.
+//  Copyright © 2018 Tinkerswitch. All rights reserved.
+//
+
+#if canImport(UIKit) && !os(watchOS) && !os(tvOS)
+
+import Foundation
+import UIKit
+
+
+public protocol CanShowPreview {
+    func openPreview(url:URL,animated:Bool)
+}
+
+
+public extension CanShowPreview where Self: UIViewController {
+    
+
+    func openPreview(url:URL, animated:Bool = true){
+        let preview = UIDocumentInteractionController(url: url)
+        
+        let vcHolder = VCDelegateForDocumentInteractionPresentation.init(viewController: self)
+        
+        //vcHolder could be deallocated before presentPreview
+        withExtendedLifetime(vcHolder) {
+            preview.delegate = vcHolder
+            preview.presentPreview(animated: animated)
+        }
+    }
+    
+}
+
+private class VCDelegateForDocumentInteractionPresentation:NSObject, UIDocumentInteractionControllerDelegate {
+    var viewController:UIViewController
+ 
+    init(viewController aVC:UIViewController){
+        viewController = aVC
+        super.init()
+    }
+    
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return viewController
+    }
+
+}
+
+#endif
