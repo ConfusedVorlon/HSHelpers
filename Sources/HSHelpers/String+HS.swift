1bd3b0f1c9d837904806caca899e5f0f4a99b2a3
Rob Jonson <rob@hobbyistsoftware.com>
2021-04-22 19:55:02 +0100

    add string range accessors

--- Sources/HSHelpers/String+HS.swift
+++ Sources/HSHelpers/String+HS.swift
@@ -19,6 +19,16 @@ public extension LosslessStringConvertible {
     }
 }
 
+//https://stackoverflow.com/questions/24092884/get-nth-character-of-a-string-in-swift-programming-language/38215613#38215613
+extension StringProtocol {
+    subscript(_ offset: Int)                     -> Element     { self[index(startIndex, offsetBy: offset)] }
+    subscript(_ range: Range<Int>)               -> SubSequence { prefix(range.lowerBound+range.count).suffix(range.count) }
+    subscript(_ range: ClosedRange<Int>)         -> SubSequence { prefix(range.lowerBound+range.count).suffix(range.count) }
+    subscript(_ range: PartialRangeThrough<Int>) -> SubSequence { prefix(range.upperBound.advanced(by: 1)) }
+    subscript(_ range: PartialRangeUpTo<Int>)    -> SubSequence { prefix(range.upperBound) }
+    subscript(_ range: PartialRangeFrom<Int>)    -> SubSequence { suffix(Swift.max(0, count-range.lowerBound)) }
+}
+
 public extension String {
     
     
