04ff16f98aa29c8f03dca548d38a67e5da745cc3
Rob Jonson <rob@hobbyistsoftware.com>
2022-09-22 16:37:28 +0100

    add isPreview test

--- Sources/HSHelpers/Types/Build.swift
+++ Sources/HSHelpers/Types/Build.swift
@@ -24,4 +24,8 @@ public struct Build {
         assert(set(debug: true))
         return isDebug
     }()
+    
+    public static let isPreview: Bool = {
+        return ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }()
 }
