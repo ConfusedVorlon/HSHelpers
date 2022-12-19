f5239af7724106e8b4b6c2ef1b46658a3604ae8e
Rob Jonson <rob@hobbyistsoftware.com>
2022-12-19 19:02:08 +0000

    add Action & MainAction aliases

---
+++ Sources/HSHelpers/Types/Aliases.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rob Jonson on 19/12/2022.
+//
+
+import Foundation
+
+public typealias Action = () -> Void
+public typealias MainAction = @MainActor () -> Void
