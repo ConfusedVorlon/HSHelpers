1d2218bc487a51fb81984f0787d403196086be01
Rob Jonson <rob@hobbyistsoftware.com>
2023-05-11 14:24:24 +0100

    remove extension flag as this makes older xcodes sad

--- Package.swift
+++ Package.swift
@@ -29,24 +29,16 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HSHelpers",
-            dependencies: [],
-            // build documentation with extensions
-            swiftSettings: [.unsafeFlags(["-emit-extension-block-symbols"])]),
+            dependencies: []),
         .target(
             name: "HSAppKit",
-            dependencies: ["HSHelpers"],
-            // build documentation with extensions
-            swiftSettings: [.unsafeFlags(["-emit-extension-block-symbols"])]),
+            dependencies: ["HSHelpers"]),
         .target(
             name: "HSUIKit",
-            dependencies: ["HSHelpers"],
-            // build documentation with extensions
-            swiftSettings: [.unsafeFlags(["-emit-extension-block-symbols"])]),
+            dependencies: ["HSHelpers"]),
         .target(
             name: "HSTime",
-            dependencies: [.product(name: "Time", package: "Time")],
-            // build documentation with extensions
-            swiftSettings: [.unsafeFlags(["-emit-extension-block-symbols"])]),
+            dependencies: [.product(name: "Time", package: "Time")]),
         .testTarget(
             name: "HSHelpersTests",
             dependencies: ["HSHelpers"])
