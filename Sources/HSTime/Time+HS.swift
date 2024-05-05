80d0bb8f5b45f0a09cc17a986fe4fab2af6e7575
Rob Jonson <rob@hobbyistsoftware.com>
2024-05-05 11:48:40 +0100

    add relative date helpers

--- Sources/HSTime/Time+HS.swift
+++ Sources/HSTime/Time+HS.swift
@@ -13,9 +13,24 @@ import Time
 
 public extension Interval {
 
+    
+    /// Allows 2.days.after(someDate)
+    /// - Parameter date: base
+    /// - Returns: new date
+    func after(_ date:Date) -> Date{
+        return date + self
+    }
+    
     // Allows 2.days.fromNow
     var fromNow: Date {
         return Date.init() + self
+    }
+    
+    /// Allows 2.days.before(someDate)
+    /// - Parameter date: base
+    /// - Returns: new date
+    func before(_ date:Date) -> Date{
+        return date - self
     }
 
     // Allows 1.hour.ago
