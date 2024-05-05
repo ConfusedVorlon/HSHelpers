9dd6a3707c393fba0dea0fec1622639d41b6ede4
Rob Jonson <rob@hobbyistsoftware.com>
2024-05-05 11:59:42 +0100

    relax version requirements for hshelpers
    aligns with hobbyist package

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "HSHelpers",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v13),
-        .tvOS(.v13)
+        .iOS(.v12),
+        .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
