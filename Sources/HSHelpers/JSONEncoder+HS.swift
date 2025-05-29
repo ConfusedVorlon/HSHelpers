2bf37919d68cac6d40d0d54da27e806408aeb850
Rob Jonson <rob@hobbyistsoftware.com>
2025-05-29 14:30:26 +0100

    updates to JSONEncoder().pretty()

--- Sources/HSHelpers/JSONEncoder+HS.swift
+++ Sources/HSHelpers/JSONEncoder+HS.swift
@@ -12,6 +12,13 @@ public extension JSONEncoder {
     // Allows let encoder = JSONEncoder().pretty()
     func pretty() -> JSONEncoder {
         self.outputFormatting.insert(.prettyPrinted)
+        self.outputFormatting.insert(.sortedKeys)
+        if #available(iOS 13.0,macOS 10.15,tvOS 13.0, *) {
+            // Unless we're embedding in html - this is better
+            self.outputFormatting.insert(.withoutEscapingSlashes)
+        } else {
+            // Fallback on earlier versions
+        }
         return self
     }
 
